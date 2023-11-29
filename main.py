bfda40cdf8cac8fe6760114333e2224228a44bfa
Anna J <ajasion5@gmail.com>
2023-11-29 00:25:08 +0000

    compare user's input with reversed user's input and print the answer

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-my_word = input('Enter your word: ')
-print(my_word)
+my_word = input('Enter your word: ').lower()
+if (my_word == my_word[::-1]):
+    print(f'Your word: "{my_word}" is a palindrome.')
+else:
+    print(f'The word: "{my_word}" is NOT a palindrome.')
