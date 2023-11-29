8aaacc0bddac1f2d6f659bae03b505c1f4c1f00d
Anna J <ajasion5@gmail.com>
2023-11-29 00:52:31 +0000

    add README

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 my_word = input('Enter your word: ').lower()
 
 def check_if_palindrome(word):
+    """
+    takes input typed by user to check for palindrome, then reverses input and checks if input and reversed input are the same strings, and then prints the relevant answer.
+    """
     if (word == word[::-1]):
         print('Your word is a palindrome.')
     else:
         print('The word is NOT a palindrome.')
 
 check_if_palindrome(my_word)
+
